ecb9ac9e4fb1a1702a1473567f27d636eb3a44bc
Nerybeth Bravo <neribravolopez@gmail.com>
2026-06-10 16:39:48 -0600

    conexión a la BD agregada

---
+++ Programación/AppRegistroHabitos/src/Conexion/Conexion.java
@@ -0,0 +1,28 @@
+
+package Conexion;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class Conexion {
+    private static final String URL
+            = "jdbc:mysql://localhost:3306/SistemaRegistroHabitos";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    
+    public static Connection conectar() {
+        Connection con = null;
+        try {
+                        con = DriverManager.getConnection(
+
+                    URL,
+                    USER,
+                    PASSWORD
+            );
+            System.out.println("Conexión exitosa");
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return con;
+    }
+}
--- Programación/AppRegistroHabitos/src/Vista/AppRegistroHabitos.java
+++ Programación/AppRegistroHabitos/src/Vista/AppRegistroHabitos.java
@@ -1,12 +1,15 @@
 
 package Vista;
 
+import Conexion.Conexion;
+
 
 public class AppRegistroHabitos {
 
     
     public static void main(String[] args) {
-        
+                Conexion.conectar();
+
     }
     
 }
